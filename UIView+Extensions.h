b8f7ed417d9df82ea9def9dea63ce3188a369fc9
Tosh Meston <tosh.meston@gmail.com>
2013-11-20 23:06:20 -0800

    Additional methods for bounds as well as frame, and logGeometry

--- UIView+Extensions.h
+++ UIView+Extensions.h
@@ -2,15 +2,21 @@
 //  UIView+Extensions.h
 //
 
-#import <UIKit/UIKit.h>
+@import UIKit;
 
 @interface UIView (Extensions)
 
-@property (nonatomic) CGFloat centerX;
-@property (nonatomic) CGFloat centerY;
-@property (nonatomic) CGFloat height;
-@property (nonatomic) CGFloat width;
-@property (nonatomic) CGFloat x;
-@property (nonatomic) CGFloat y;
+@property (nonatomic) CGFloat boundsHeight;
+@property (nonatomic) CGFloat boundsWidth;
+@property (nonatomic) CGFloat boundsX;
+@property (nonatomic) CGFloat boundsY;
+@property (nonatomic) CGFloat frameCenterX;
+@property (nonatomic) CGFloat frameCenterY;
+@property (nonatomic) CGFloat frameHeight;
+@property (nonatomic) CGFloat frameWidth;
+@property (nonatomic) CGFloat frameX;
+@property (nonatomic) CGFloat frameY;
+
+- (void)logGeometry;
 
 @end
